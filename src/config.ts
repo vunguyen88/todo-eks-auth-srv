d9b86ac82b2b0d3d24756f6f0583df0427bf8319
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-05-13 11:42:26 -0400

    update pipeline

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,9 @@ const config = {
     //Provide details for local configuration
   },
   aws_remote_config: {
-    accessKeyId: process.env.AWS_IAM_DYNAMODB_ACCESSKEY_LOCAL,
-    secretAccessKey: process.env.AWS_IAM_DYNAMODB_SECRETKEY_LOCAL,
-    region: process.env.AWS_DYNAMODB_REGION,
+    accessKeyId: process.env.AWS_IAM_DYNAMO_DB_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_IAM_DYNAMO_DB_SECRET_KEY,
+    region: process.env.AWS_DYNAMO_DB_REGION,
   }
 };
 
