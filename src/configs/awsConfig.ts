25009e5f6f386e4d7b42e2b2cea0987590f1bcf7
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-05-28 13:23:05 -0400

    updat env

--- src/configs/awsConfig.ts
+++ src/configs/awsConfig.ts
@@ -3,9 +3,10 @@ import dotenv from 'dotenv';
 import { DynamoDB } from 'aws-sdk';
 
 dotenv.config()
-
+console.log('AWS_IAM_AUTH_SERVICE_ACCESS_KEY', process.env.AWS_IAM_AUTH_SERVICE_ACCESS_KEY)
+console.log('region ', process.env.AWS_REGION)
 // Set the region
-AWS.config.update({ region: process.env.AWS_REGION });
+AWS.config.update({ region: 'us-east-2' });
 const dynamoDB = new DynamoDB.DocumentClient({
   accessKeyId: process.env.AWS_IAM_AUTH_SERVICE_ACCESS_KEY,
   secretAccessKey: process.env.AWS_IAM_AUTH_SERVICE_SECRET_KEY,
